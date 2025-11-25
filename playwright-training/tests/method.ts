862e3821bc1eff69e96c3ac25f58c557b11d2a6e
ssikkan <ssikkan@gmail.com>
2025-11-25 23:45:19 +0000

    function fix

--- playwright-training/tests/method.ts
+++ playwright-training/tests/method.ts
@@ -1,25 +1,25 @@
-function greet(){
+function greet() {
     console.log("Hello, welcome to Playwright training!");
 }
 
 greet();
 
 //function with parameters
-function greetuser(name: string){
-    console.log("Hello $(name), welcome to Playwright training!");
+function greetuser(name: string) {
+    console.log(`Hello ${name}, welcome to Playwright training!`);
 }
 greetuser("John");
 
 //function with return type
-function addnumbers(a:number, b:number):number{
-    return a+b
+function addnumbers(a: number, b: number): number {
+    return a + b
 }
-let sum = addnumbers(5,10);
+let sum = addnumbers(5, 10);
 
 //function with optional parameter
-function multiplynumbers(a:number, b:number, c?:number):number{
-    if(c){
-        return a*b*c;
+function multiplynumbers(a: number, b: number, c?: number): number {
+    if (c) {
+        return a * b * c;
     }
-    return a*b;
+    return a * b;
 }
