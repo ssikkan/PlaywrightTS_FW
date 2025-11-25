265474efc28765f94838755dce6cc913d2e6e024
ssikkan <ssikkan@gmail.com>
2025-11-25 23:05:37 +0000

    first commit for the changes

---
+++ playwright-training/tests/variables.ts
@@ -0,0 +1,14 @@
+let age:number = 25;
+const country:string = "India";
+//country="USA"; // This will cause an error because 'country' is a constant
+
+age=30; // This is valid since 'age' is declared with 'let'
+age=22.5; // This is also valid
+let city="New York";
+let bool=true;
+let person:any
+let array1=[1,2,3];
+let array11:number[]=[1,2,3];
+let array2:string[]=["a","b","c"];
+let array3:any[]=[1,"b",true];
+let persondetails:{name:string;ages:number}[]=[];
